ee4c309530f44d99ed312ec9b94b72c9db46e06a
whyleonardo <pleasefloyd73@gmail.com>
2024-01-09 12:20:19 -0300

    chore: define app theme colors

--- build-globals-edit-css.js
+++ build-globals-edit-css.js
@@ -1,15 +1,15 @@
-const { readFileSync, writeFileSync } = require('fs')
-const path = require('path')
+const { readFileSync, writeFileSync } = require("fs")
+const path = require("path")
 
-const CSS_ROOT = 'src/styles'
+const CSS_ROOT = "src/styles"
 
-const file = readFileSync(path.join(__dirname, CSS_ROOT, 'global.css'), {
-	encoding: 'utf-8',
+const file = readFileSync(path.join(__dirname, CSS_ROOT, "global.css"), {
+	encoding: "utf-8",
 })
 
 const fixed = file.replaceAll(
 	/(.*--.+:)[ ]*(.+[ ]+.+%[ ]+.+%);/g,
-	'$1 hsl($2);',
+	"$1 hsl($2);",
 )
 
-writeFileSync(path.join(__dirname, CSS_ROOT, 'global-edit.css'), fixed)
+writeFileSync(path.join(__dirname, CSS_ROOT, "global-edit.css"), fixed)
