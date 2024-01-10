3a39c9541662787c501a51e2aa9ca7960d92f60e
whyleonardo <pleasefloyd73@gmail.com>
2024-01-10 11:39:42 -0300

    feat: add font poppins

--- src/styles/fonts/index.ts
+++ src/styles/fonts/index.ts
@@ -1,3 +1,4 @@
+import { Poppins } from "next/font/google"
 import localFont from "next/font/local"
 
 import { GeistMono, GeistSans } from "geist/font"
@@ -6,7 +7,13 @@ const localCalSans = localFont({
 	src: "./cal-sans/CalSans-SemiBold.woff2",
 	variable: "--font-cal-sans",
 })
+const poppins = Poppins({
+	weight: ["400", "500", "600"],
+	subsets: ["latin"],
+	variable: "--font-poppins",
+})
 
+export const fontPoppins = poppins.variable
 export const fontCalSans = localCalSans.variable
 export const fontSans = GeistSans.variable
 export const fontMono = GeistMono.variable
