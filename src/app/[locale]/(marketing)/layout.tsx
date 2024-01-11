0cde4ec0d21b4808051d364d40794e22bf5b812a
whyleonardo <pleasefloyd73@gmail.com>
2024-01-11 16:03:50 -0300

    chore: remove unused clerk logic

--- src/app/[locale]/(marketing)/layout.tsx
+++ src/app/[locale]/(marketing)/layout.tsx
@@ -1,22 +1,8 @@
-"use client"
-
-import { useRouter } from "next/navigation"
-import { usePathname } from "next/navigation"
-
-import { useAuth } from "@clerk/nextjs"
-
 interface MarketingLayoutProps {
 	children: React.ReactNode
 }
 
 const MarketingLayout = ({ children }: MarketingLayoutProps) => {
-	const { userId } = useAuth()
-	const { push } = useRouter()
-	const pathname = usePathname()
-
-	if (userId && pathname.match("/")) {
-		push("/app")
-	}
 	return <div className="h-dvh">{children}</div>
 }
 
