ae2af4fa86111ea965881322bec37ee080fc73b4
whyleonardo <pleasefloyd73@gmail.com>
2024-01-11 10:36:42 -0300

    feat: merge next-intl with clerk

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,30 @@
 import createMiddleware from "next-intl/middleware"
 
 import { defaultLocale, locales, localePrefix } from "@/constants/locales"
+import { authMiddleware } from "@clerk/nextjs"
 
-export default createMiddleware({
+const intlMiddleware = createMiddleware({
 	locales,
 	localePrefix,
 	localeDetection: true,
 	defaultLocale,
 })
 
+export default authMiddleware({
+	beforeAuth(request) {
+		return intlMiddleware(request)
+	},
+
+	publicRoutes: ["/", "/sign-in", "/sign-up"],
+})
+
 export const config = {
 	// Match only internationalized pathnames
-	matcher: ["/", "/(en|br|es)/:path*"],
+	matcher: [
+		"/",
+		"/:path*",
+		"/(en|br|es)/:path*",
+		"/((?!.+\\.[\\w]+$|_next).*)",
+		"/(api|trpc)(.*)",
+	],
 }
