c2757b2f0e45c5f6af92ff054bcd18efc6171216
whyleonardo <pleasefloyd73@gmail.com>
2024-01-11 09:35:01 -0300

    chore: add new env variables

--- src/lib/env.mjs
+++ src/lib/env.mjs
@@ -2,15 +2,20 @@ import { createEnv } from "@t3-oss/env-nextjs"
 import { z } from "zod"
 
 export const env = createEnv({
-	server: {},
+	server: {
+		CLERK_SECRET_KEY: z.string().min(1),
+	},
 	client: {
 		SITE_BASE_URL: z.string().url().min(1),
 		APP_NAME: z.string().min(1),
+		CLERK_PUBLISHABLE_KEY: z.string().min(1),
 	},
 	// If you're using Next.js < 13.4.4, you'll need to specify the runtimeEnv manually
 	runtimeEnv: {
 		SITE_BASE_URL: process.env.NEXT_PUBLIC_SITE_BASE_URL,
 		APP_NAME: process.env.NEXT_PUBLIC_APP_NAME,
+		CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+		CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
 	},
 	// For Next.js >= 13.4.4, you only need to destructure client variables:
 	// experimental__runtimeEnv: {
