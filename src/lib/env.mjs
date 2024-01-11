780fdc6dc7f05aad6d958cb5c6b38eb873080a0e
whyleonardo <pleasefloyd73@gmail.com>
2024-01-11 10:36:59 -0300

    chore: add new clerk env variables

--- src/lib/env.mjs
+++ src/lib/env.mjs
@@ -9,6 +9,10 @@ export const env = createEnv({
 		SITE_BASE_URL: z.string().url().min(1),
 		APP_NAME: z.string().min(1),
 		CLERK_PUBLISHABLE_KEY: z.string().min(1),
+		CLERK_SIGN_IN_URL: z.string().min(1),
+		CLERK_SIGN_UP_URL: z.string().min(1),
+		CLERK_AFTER_SIGN_IN_URL: z.string().min(1),
+		CLERK_AFTER_SIGN_UP_URL: z.string().min(1),
 	},
 	// If you're using Next.js < 13.4.4, you'll need to specify the runtimeEnv manually
 	runtimeEnv: {
@@ -16,6 +20,10 @@ export const env = createEnv({
 		APP_NAME: process.env.NEXT_PUBLIC_APP_NAME,
 		CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
 		CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+		CLERK_SIGN_IN_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL,
+		CLERK_SIGN_UP_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL,
+		CLERK_AFTER_SIGN_IN_URL: process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL,
+		CLERK_AFTER_SIGN_UP_URL: process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL,
 	},
 	// For Next.js >= 13.4.4, you only need to destructure client variables:
 	// experimental__runtimeEnv: {
