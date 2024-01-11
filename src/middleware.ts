2ad0855d8d34ba12efcd1e751d6fa69d48db391c
whyleonardo <pleasefloyd73@gmail.com>
2024-01-11 16:02:43 -0300

    feat: increment route protections

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,8 @@
 import createMiddleware from "next-intl/middleware"
+import { NextResponse } from "next/server"
 
 import { defaultLocale, locales, localePrefix } from "@/constants/locales"
-import { authMiddleware } from "@clerk/nextjs"
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs"
 
 const intlMiddleware = createMiddleware({
 	locales,
@@ -13,6 +14,39 @@ const intlMiddleware = createMiddleware({
 export default authMiddleware({
 	beforeAuth(request) {
 		return intlMiddleware(request)
+	},
+	afterAuth(auth, req) {
+		const response = intlMiddleware(req)
+		const actualLocale = req.cookies.get("NEXT_LOCALE")?.value
+
+		if (auth.userId && auth.isPublicRoute) {
+			let path = `/${actualLocale}/select-org`
+
+			if (auth.orgId) {
+				path = `/${actualLocale}/app/organization/${auth.orgId}`
+			}
+
+			const orgSelection = new URL(path, req.url)
+
+			response.headers.set("x-middleware-rewrite", orgSelection.toString())
+
+			return response
+		}
+
+		if (!auth.userId && !auth.isPublicRoute) {
+			return redirectToSignIn({ returnBackUrl: req.url })
+		}
+
+		if (
+			auth.userId &&
+			!auth.orgId &&
+			req.nextUrl.pathname !== `${actualLocale}/select-org`
+		) {
+			const orgSelection = new URL(`${actualLocale}/select-org`, req.url)
+			response.headers.set("x-middleware-rewrite", orgSelection.toString())
+
+			return response
+		}
 	},
 
 	publicRoutes: ["/", "/sign-in", "/sign-up"],
