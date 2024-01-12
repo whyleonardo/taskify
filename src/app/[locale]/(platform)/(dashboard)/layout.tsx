303bb1a71639b58f21f85883fcbf26d6ad4d046b
whyleonardo <pleasefloyd73@gmail.com>
2024-01-12 10:41:49 -0300

    feat: create dashboard layout

---
+++ src/app/[locale]/(platform)/(dashboard)/layout.tsx
@@ -0,0 +1,16 @@
+import { DashboardNavbar } from "./_components/navbar"
+
+interface DashboardLayoutProps {
+	children: React.ReactNode
+}
+
+const DashboardLayout = ({ children }: DashboardLayoutProps) => {
+	return (
+		<div className="h-full">
+			<DashboardNavbar />
+			{children}
+		</div>
+	)
+}
+
+export default DashboardLayout
