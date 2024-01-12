3c752a0e706285e909b128dd2a43b92428a4b4dd
whyleonardo <pleasefloyd73@gmail.com>
2024-01-12 18:16:07 -0300

    feat: create dashboard page

---
+++ src/app/[locale]/(platform)/(dashboard)/board/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import { LocaleFlagSwitcher } from "@/components/locale-flag-switcher"
+
+const DashboardPage = () => {
+	return (
+		<>
+			<LocaleFlagSwitcher />
+		</>
+	)
+}
+
+export default DashboardPage
