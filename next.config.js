926e575d3b1c4fc110013e91138645c9c938b1a2
whyleonardo <pleasefloyd73@gmail.com>
2024-01-12 17:20:12 -0300

    chore: add clerk images host

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@ const withNextIntl = require("next-intl/plugin")()
 
 module.exports = withNextIntl({
 	reactStrictMode: true,
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "img.clerk.com",
+			},
+		],
+	},
 	experimental: {
 		typedRoutes: true,
 	},
