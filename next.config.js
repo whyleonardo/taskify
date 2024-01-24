6711fcbd8b271b7297bf679ae6c2177674a76fe0
whyleonardo <pleasefloyd73@gmail.com>
2024-01-24 14:43:59 -0300

    chore: add unsplash images host

--- next.config.js
+++ next.config.js
@@ -8,6 +8,10 @@ module.exports = withNextIntl({
 				protocol: "https",
 				hostname: "img.clerk.com",
 			},
+			{
+				protocol: "https",
+				hostname: "images.unsplash.com",
+			},
 		],
 	},
 	experimental: {
