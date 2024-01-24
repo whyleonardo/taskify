08337f874d21efff19ec8d2facfce7ff30e185e8
whyleonardo <pleasefloyd73@gmail.com>
2024-01-24 14:49:49 -0300

    chore: fix link href

--- src/components/locale-flag-switcher.tsx
+++ src/components/locale-flag-switcher.tsx
@@ -4,7 +4,7 @@ import { useLocale } from "next-intl"
 import ReactCountryFlag from "react-country-flag"
 
 import { locales } from "@/constants/locales"
-import { Link } from "@/lib/navigation"
+import { Link, usePathname } from "@/lib/navigation"
 import { cn } from "@/lib/utils"
 
 interface LocaleSwitcherProps {
@@ -13,19 +13,19 @@ interface LocaleSwitcherProps {
 
 export const LocaleFlagSwitcher = ({ className }: LocaleSwitcherProps) => {
 	const actualLocale = useLocale()
-
+	const p = usePathname()
 	return (
 		<>
 			<div className="flex items-center gap-2">
 				{locales.map((locale) => (
-					<Link href="" locale={locale} key={locale}>
+					<Link href={p} locale={locale} key={locale}>
 						<ReactCountryFlag
 							countryCode={locale === "en" ? "us" : locale}
 							lang={locale}
 							svg
 							aria-label={`Switch to ${locale} locale`}
 							className={cn(
-								"!h-8 !w-8",
+								"!h-8 !w-8 rounded-sm",
 								locale !== actualLocale && "grayscale",
 								className,
 							)}
