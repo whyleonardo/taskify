068aa3da2bd28ac561aea43b18e80c8ce9d3724e
whyleonardo <pleasefloyd73@gmail.com>
2024-01-24 14:46:27 -0300

    feat: add unsplash keys env variables

--- src/lib/env.mjs
+++ src/lib/env.mjs
@@ -4,8 +4,10 @@ import { z } from "zod"
 export const env = createEnv({
 	server: {
 		CLERK_SECRET_KEY: z.string().min(1),
+		UNSPLASH_SECRET_KEY: z.string().min(1),
 	},
 	client: {
+		NEXT_PUBLIC_UNSPLASH_ACCESS_KEY: z.string().min(1),
 		NEXT_PUBLIC_SITE_BASE_URL: z.string().url().min(1),
 		NEXT_PUBLIC_APP_NAME: z.string().min(1),
 		NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1),
@@ -16,6 +18,9 @@ export const env = createEnv({
 	},
 	// If you're using Next.js < 13.4.4, you'll need to specify the runtimeEnv manually
 	runtimeEnv: {
+		NEXT_PUBLIC_UNSPLASH_ACCESS_KEY:
+			process.env.NEXT_PUBLIC_UNSPLASH_ACCESS_KEY,
+		UNSPLASH_SECRET_KEY: process.env.UNSPLASH_SECRET_KEY,
 		NEXT_PUBLIC_SITE_BASE_URL: process.env.NEXT_PUBLIC_SITE_BASE_URL,
 		NEXT_PUBLIC_APP_NAME: process.env.NEXT_PUBLIC_APP_NAME,
 		NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
