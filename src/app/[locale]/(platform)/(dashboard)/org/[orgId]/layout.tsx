b1bbd37f074258fe74220ca9c443c86eab79c89f
whyleonardo <pleasefloyd73@gmail.com>
2024-01-30 11:30:51 -0300

    feat: add dynamic metadata based on org info

--- src/app/[locale]/(platform)/(dashboard)/org/[orgId]/layout.tsx
+++ src/app/[locale]/(platform)/(dashboard)/org/[orgId]/layout.tsx
@@ -1,7 +1,30 @@
 import { OrgControl } from "./_components/org-control"
 
+import { auth } from "@clerk/nextjs"
+import { startCase } from "lodash"
+
 interface OrganizationIdProps {
 	children: React.ReactNode
+}
+
+export async function generateMetadata({
+	params: { locale },
+}: {
+	params: { locale: string }
+}) {
+	const { orgSlug } = auth()
+
+	if (locale === "en") {
+		return {
+			title: startCase(orgSlug || "organization"),
+		}
+	}
+
+	if (locale === "br") {
+		return {
+			title: startCase(orgSlug || "organização"),
+		}
+	}
 }
 
 const OrganizationIdLayout = ({ children }: OrganizationIdProps) => {
