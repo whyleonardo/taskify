125ff969ca48dfb3493c8db51f73844b65c0f062
Christian Leonardo <pleasefloyd73@gmail.com>
2024-03-19 20:05:52 -0300

    chore: adjust some footer styles

--- src/app/(marketing)/_components/footer.tsx
+++ src/app/(marketing)/_components/footer.tsx
@@ -7,16 +7,18 @@ export const MarketingFooter = async () => {
 		<footer className="flex h-16 w-full border-t shadow-lg">
 			<div className="container flex items-center justify-between">
 				<div className="flex items-center justify-between gap-2 md:w-fit md:justify-normal">
-					<span className="text-xs">
-						Built &nbsp;
+					<p className="text-xs">
+						Built by&nbsp;
 						<Link
 							href="https://github.com/whyleonardo"
 							className="underline transition-all hover:text-foreground/70"
 						>
 							whyleonardo
 						</Link>
-					</span>
+					</p>
+				</div>
 
+				<div className="ml-auto">
 					<ThemeToggle />
 				</div>
 			</div>
